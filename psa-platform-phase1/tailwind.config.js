1f5db95cd2d5079181e4e3dd6734153e4d315d84
consultiservice-star <consultiservice@gmail.com>
2026-08-01 12:27:57 -0400

    Update tailwind.config.js

--- psa-platform-phase1/tailwind.config.js
+++ psa-platform-phase1/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/[(]auth[)]/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/[(]dashboard[)]/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
