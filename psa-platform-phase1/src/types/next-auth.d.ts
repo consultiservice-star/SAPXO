67bd2b59709a0bc64da8d968c72fcb90a0e369d2
consultiservice-star <consultiservice@gmail.com>
2026-08-01 01:17:39 -0400

    Update next-auth.d.ts

--- psa-platform-phase1/src/types/next-auth.d.ts
+++ psa-platform-phase1/src/types/next-auth.d.ts
@@ -1,1 +1,28 @@
+import { Role } from "@prisma/client";
+import "next-auth";
+import "next-auth/jwt";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      email: string;
+      name: string;
+      tenantId: string;
+      tenantSlug: string;
+      role: Role;
+      clientId?: string;
+      locale: string;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    tenantId: string;
+    tenantSlug: string;
+    role: Role;
+    clientId?: string;
+    locale: string;
+  }
+}
